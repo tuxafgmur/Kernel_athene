2a26a2d44a6cb376b9cd7c9a7d1fa8895880d7ea
tuxafgmur <tuxafgmur@users.noreply.github.com>
2017-04-05 02:44:09 +0200

    Reduces binder logs

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -132,19 +132,8 @@ static int binder_set_stop_on_user_error(const char *val,
 module_param_call(stop_on_user_error, binder_set_stop_on_user_error,
 	param_get_int, &binder_stop_on_user_error, S_IWUSR | S_IRUGO);
 
-#define binder_debug(mask, x...) \
-	do { \
-		if (binder_debug_mask & mask) \
-			pr_info(x); \
-	} while (0)
-
-#define binder_user_error(x...) \
-	do { \
-		if (binder_debug_mask & BINDER_DEBUG_USER_ERROR) \
-			pr_info(x); \
-		if (binder_stop_on_user_error) \
-			binder_stop_on_user_error = 2; \
-	} while (0)
+#define binder_debug(mask, x...) do {} while (0)
+#define binder_user_error(x...)  do {} while (0)
 
 enum binder_stat_types {
 	BINDER_STAT_PROC,
